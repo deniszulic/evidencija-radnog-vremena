e8f756e913088fe9668c3d1b64f878b66682c321
ExistenceisP4ain <57858720+ExistenceisP4ain@users.noreply.github.com>
2021-12-26 13:34:56 +0100

    zakljucano page

--- frontend/src/views/zakljucano.vue
+++ frontend/src/views/zakljucano.vue
@@ -1,9 +1,40 @@
 <template>
   <div>
+    <div class="jumbotron">
+  <h1 class="display-4">Profil</h1>
+  <hr>
+</div>
     <!-- {{podaci}} -->
+    <table class="table">
+  <thead class="thead-dark">
+      <td>#</td>
+      <td>Razdoblje</td>
+      <td>Status</td>
+      <td></td>
+  </thead>
+  <tbody>
+    <tr v-for="(data,index) in podaci" :key="data.id" >
+          
+         <td class="">{{index+1}}. </td>
+         <td class="col-md-7">{{moment(data.datum_obavljanja_pocetak).format("DD.MM.YYYY")}} - {{moment(data.datum_obavljanja_kraj).format("DD.MM.YYYY")}} </td>
+
+          <td class="col-md-2">
+            <b>Zaključano</b>
+          </td>
+<td class="col-md-1">
+           <button class="btn btn-light"><img src='https://svgshare.com/i/d3E.svg' width=20% height=20% /></button>
+          </td>
+        </tr>
+  </tbody>
+</table>
+
+
+
+
   </div>
 </template>
 <script>
+import moment from "moment";
 import {dohvatpodataka} from '@/services'
 import {Auth} from '@/services'
 export default {
@@ -11,6 +42,7 @@ export default {
   data() {
     return {
       podaci: [],
+      moment
     };
   },
   created() {
@@ -23,6 +55,11 @@ export default {
     async fetchData(){
       this.podaci=await dohvatpodataka.getlockeddata(Auth.state.id)
     }
+  },
+  filters: {
+    moment: function (date) {
+      return moment(date).format("MMMM");
+    },
   }
 };
 </script>
